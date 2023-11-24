a3661f40a0270931328c67d52cd5ba8bdc1e1730
Nick Fox-Gieg <nick@fox-gieg.com>
2023-11-24 00:45:10 -0500

    test

--- openframeworks/MagicLeapt/src/ofApp.cpp
+++ openframeworks/MagicLeapt/src/ofApp.cpp
@@ -4,6 +4,15 @@ void ofApp::setup() {
     snd.load("sound.mp3");
     snd.setLoop(true);
     snd.play();
+    
+    int length = sizeof(timecodeSourceArray) / sizeof(Timecode);
+
+    for (int i=0; i<length; i++) {
+        Timecode t;
+        t.startTime = timecodeSourceArray[i].startTime;
+        t.stopTime = timecodeSourceArray[i].stopTime * offsetPercent;
+        timecodeArray.push_back(t);
+    }
 }
 
 void ofApp::update() {
